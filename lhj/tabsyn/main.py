7db306e9c269f68768a65621a5715fe1c431bed9
ZeroActLee <lhj56822@gmail.com>
2025-05-25 11:39:00 +0900

    add tabsyn

---
+++ lhj/tabsyn/main.py
@@ -0,0 +1,18 @@
+import torch
+from utils import execute_function, get_args
+
+if __name__ == '__main__':
+    args = get_args()
+    if torch.cuda.is_available():
+        args.device = f'cuda:{args.gpu}'
+    else:
+        args.device = 'cpu'
+    
+    __spec__ = None
+    print(f'Using device: {args.device}')
+
+    if not args.save_path:
+        args.save_path = f'synthetic/{args.dataname}/{args.method}.csv'
+    main_fn = execute_function(args.method, args.mode)
+
+    main_fn(args)
